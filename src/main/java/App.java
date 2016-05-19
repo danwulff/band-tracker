7966f161280c4ba55b1a0239423d21c57a2b2b6b
Daniel Wulff <danielwulff.ee@gmail.com>
2016-05-18 23:31:19 -0700

    created apptests for Band pages and Venue page, routing created and passes tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,91 @@ import static spark.Spark.*;
 public class App {
 
   public static void main (String[] args){
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get ("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get ("/bands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("bands", Band.all());
+      model.put("template", "templates/bands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post ("/bands", (request, response) -> {
+      String name = request.queryParams("name");
+      Band newBand = new Band(name);
+      newBand.save();
+      response.redirect("/bands");
+      return null;
+    });
+
+    get ("/bands/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Band thisBand = Band.find(id);
+      model.put("band", thisBand);
+      model.put("venues", Venue.all());
+      model.put("template", "templates/band.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post ("/add_venue", (request, response) -> {
+      int bandId = Integer.parseInt(request.queryParams("band_id"));
+      int venueId = Integer.parseInt(request.queryParams("venue_id"));
+      Band thisBand = Band.find(bandId);
+      thisBand.addVenue(Venue.find(venueId));
+      String url = String.format("http://localhost:4567/bands/%d", bandId);
+      response.redirect(url);
+      return null;
+    });
+
+    get ("/bands/:id/edit", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Band thisBand = Band.find(id);
+      model.put("band", thisBand);
+      model.put("template", "templates/band-edit.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post ("/bands/:id", (request, response) -> {
+      int id = Integer.parseInt(request.params("id"));
+      Band thisBand = Band.find(id);
+      String name = request.queryParams("name");
+      thisBand.update(name);
+      String url = String.format("http://localhost:4567/bands/%d", id);
+      response.redirect(url);
+      return null;
+    });
+
+    post ("/bands/:id/delete", (request, response) -> {
+      int id = Integer.parseInt(request.params("id"));
+      Band thisBand = Band.find(id);
+      thisBand.delete();
+      response.redirect("http://localhost:4567/bands");
+      return null;
+    });
+
+    get ("/venues", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("venues", Venue.all());
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post ("/venues", (request, response) -> {
+      String name = request.queryParams("name");
+      Venue newVenue = new Venue(name);
+      newVenue.save();
+      response.redirect("/venues");
+      return null;
+    });
 
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,11 @@
+import org.sql2o.*;
+import org.junit.*;
 import org.fluentlenium.adapter.FluentTest;
-import org.junit.ClassRule;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -16,9 +18,112 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("");
-  // }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("band tracker");
+  }
+
+  @Test
+  public void bandsPageDisplaysBandName() {
+    Band testBand = new Band("Citizen Cope");
+    testBand.save();
+    goTo("http://localhost:4567/bands");
+    assertThat(pageSource()).contains("Citizen Cope");
+  }
+
+  @Test
+  public void bandIsCreatedTest() {
+    goTo("http://localhost:4567/bands");
+    fill("#name").with("Citizen Cope");
+    submit(".btn");
+    assertThat(pageSource()).contains("Citizen Cope");
+  }
+
+  @Test
+  public void individualBandPageDisplaysBandName() {
+    Band testBand = new Band("Citizen Cope");
+    testBand.save();
+    String url = String.format("http://localhost:4567/bands/%d", testBand.getId());
+    goTo(url);
+    assertThat(pageSource()).contains("Citizen Cope");
+  }
+
+  @Test
+  public void individualBandPageDisplaysConnectedVenues() {
+    Band testBand = new Band("Citizen Cope");
+    testBand.save();
+    Venue testVenue = new Venue("Carnegie");
+    testVenue.save();
+    testBand.addVenue(testVenue);
+    String url = String.format("http://localhost:4567/bands/%d", testBand.getId());
+    goTo(url);
+    assertThat(pageSource()).contains("Carnegie");
+  }
+
+  @Test
+  public void individualBandPageAddsVenues() {
+    Band testBand = new Band("Citizen Cope");
+    testBand.save();
+    Venue testVenue = new Venue("Carnegie");
+    testVenue.save();
+    String url = String.format("http://localhost:4567/bands/%d", testBand.getId());
+    goTo(url);
+    fillSelect("#venue_id").withText("Carnegie");
+    submit(".btn");
+    assertThat(pageSource()).contains("<li>");
+    assertThat(pageSource()).contains("Carnegie");
+  }
+
+  @Test
+  public void bandEditPageDisplays() {
+    Band testBand = new Band("Citizen Cope");
+    testBand.save();
+    String url = String.format("http://localhost:4567/bands/%d/edit", testBand.getId());
+    goTo(url);
+    assertThat(pageSource()).contains("Edit Citizen Cope");
+  }
+
+
+  @Test
+  public void bandUpdateChangesName() {
+    Band testBand = new Band("Citizen Cope");
+    testBand.save();
+    String url = String.format("http://localhost:4567/bands/%d/edit", testBand.getId());
+    goTo(url);
+    fill("#name").with("Foxy Shazam");
+    click("button", withText("Update name"));
+    assertThat(pageSource()).contains("Foxy Shazam");
+  }
+
+  @Test
+  public void bandDeleteRemovesBand() {
+    Band testBand = new Band("Citizen Cope");
+    testBand.save();
+    String url = String.format("http://localhost:4567/bands/%d/edit", testBand.getId());
+    goTo(url);
+    click("button", withText("Delete"));
+    assertEquals(0, Band.all().size());
+  }
+
+  //Venue pages tests------------------------------------------
+  @Test
+  public void venuesPageDisplaysVenueName() {
+    Venue testVenue = new Venue("Carnegie");
+    testVenue.save();
+    goTo("http://localhost:4567/venues");
+    assertThat(pageSource()).contains("Carnegie");
+  }
+
+  @Test
+  public void venueIsCreatedTest() {
+    goTo("http://localhost:4567/venues");
+    fill("#name").with("Carnegie");
+    submit(".btn");
+    assertThat(pageSource()).contains("Carnegie");
+  }
 
 }
